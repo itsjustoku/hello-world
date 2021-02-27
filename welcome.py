0824314e4bdd33c0f4c4284437750cbb16654def
Kaku Baruah <46296585+SulbulPandey@users.noreply.github.com>
2021-02-27 12:13:34 +0530

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,9 @@
+import os
+import time
+print('Hey dude, what's your name ?')
+time.sleep(1)
+name = input()
+time.sleep(1)
+print('Hey ' + name + ',welcome to my program !')
+time.sleep(1)
+print('Have a nice day ahead,' + name)
